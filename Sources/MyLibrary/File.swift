f477feb5be3d2827a9ca9e08b5b84f9e4383fdff
forwardto9 <forwarto9@hotmail.com>
2021-03-16 19:08:01 +0800

    add accessing to images

---
+++ Sources/MyLibrary/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by uwei on 2021/3/16.
+//
+
+import Foundation
+
+#if canImport(UIKit)
+import UIKit
+
+public let icon = UIImage(named: "icon58", in: Bundle.module, compatibleWith: nil)
+
+#endif
