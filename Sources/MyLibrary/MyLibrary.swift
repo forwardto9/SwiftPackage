1ca7f040e0f53811ee5b5c39821bc21e2d18e8c8
forwardto9 <forwarto9@hotmail.com>
2021-03-16 22:54:23 +0800

    update

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 public struct MyLibrary {
-   public var text = "Hello, World!"
+    var text = "Hello, World!"
     // Test Resources
-    public let picture = Bundle.module.path(forResource: "icon58", ofType: "png")
+    public static let picture = Bundle.module.path(forResource: "icon58", ofType: "png")
     public static func showName() -> Void {
         print("this is My Library Name method")
     }
